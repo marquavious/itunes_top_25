3dd9393e20181d449d5b4ddb3b84b5854966f49b
Marquavious Draggon <marq.draggon@gmail.com>
2017-05-03 20:47:56 -0700

    init

---
+++ makeschool_coding_challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  makeschool_coding_challenge
+//
+//  Created by Marquavious on 5/3/17.
+//  Copyright © 2017 Marquavious Draggon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
